74ddb85b42241387c5ce94b5af8421391e110d59
jongyunha <plaque123@daum.net>
2026-08-10 19:21:56 +0900

    add constants and registry implementation for dependency injection

--- stores/module.go
+++ stores/module.go
@@ -2,9 +2,15 @@ package stores
 
 import (
 	"context"
+	"goedd/internal/di"
 	"goedd/internal/system"
+	"goedd/stores/internal/constants"
 )
 
 func Root(ctx context.Context, svc system.Service) (err error) {
-	return // TODO
+	container := di.New()
+	// setup Driven adapters
+	container.AddSingleton(constants.RegistryKey, func(c di.Container) (any, error) {
+		reg := registry.New()
+	})
 }
