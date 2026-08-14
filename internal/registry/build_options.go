58c3a76b17e48c84febb203e49ee8a99e5ff3e75
jongyunha <plaque123@daum.net>
2026-08-14 17:56:32 +0900

    catalog, mall repository 추가

--- internal/registry/build_options.go
+++ internal/registry/build_options.go
@@ -1,3 +1,30 @@
 package registry
 
+import (
+	"fmt"
+	"reflect"
+)
+
 type BuildOption func(v any) error
+
+func ValidateImplements(checkV any) BuildOption {
+	checkT := reflect.TypeOf(checkV)
+
+	if checkT.Kind() == reflect.Pointer {
+		checkT = checkT.Elem()
+	}
+
+	if checkT.Kind() != reflect.Interface {
+		panic(fmt.Sprintf("%T is not an interface", checkV))
+	}
+
+	return func(v any) error {
+		t := reflect.TypeOf(v)
+
+		if !t.Implements(checkT) {
+			return fmt.Errorf("%T does not implement %T", v, checkV)
+		}
+
+		return nil
+	}
+}
--- stores/module.go
+++ stores/module.go
@@ -3,6 +3,8 @@ package stores
 import (
 	"context"
 	"database/sql"
+	"goedd/stores/internal/domain"
+	"goedd/stores/internal/postgres"
 
 	"goedd/internal/am"
 	"goedd/internal/amotel"
@@ -67,7 +69,36 @@ func Root(ctx context.Context, svc system.Service) (err error) {
 	container.AddScoped(constants.AggregateStoreKey, func(c di.Container) (any, error) {
 		tx := postgresotel.Trace(c.Get(constants.DatabaseTransactionKey).(*sql.Tx))
 		reg := c.Get(constants.RegistryKey).(registry.Registry)
-		return es.NewAggregateStoreWithMiddleware(), nil
+		return es.AggregateStoreWithMiddleware(
+			pg.NewEventStore(constants.EventsTableName, tx, reg),
+			pg.NewSnapshotStore(constants.SnapshotsTableName, tx, reg),
+		), nil
+	})
+	container.AddScoped(constants.StoresRepoKey, func(c di.Container) (any, error) {
+		return es.NewAggregateRepository[*domain.Store](
+			domain.StoreAggregate,
+			c.Get(constants.RegistryKey).(registry.Registry),
+			c.Get(constants.AggregateStoreKey).(es.AggregateStore),
+		), nil
+	})
+	container.AddScoped(constants.ProductsRepoKey, func(c di.Container) (any, error) {
+		return es.NewAggregateRepository[*domain.Product](
+			domain.ProductAggregate,
+			c.Get(constants.RegistryKey).(registry.Registry),
+			c.Get(constants.AggregateStoreKey).(es.AggregateStore),
+		), nil
+	})
+	container.AddScoped(constants.CatalogRepoKey, func(c di.Container) (any, error) {
+		return postgres.NewCatalogRepository(
+			constants.CatalogTableName,
+			postgresotel.Trace(c.Get(constants.DatabaseTransactionKey).(*sql.Tx)),
+		), nil
+	})
+	container.AddScoped(constants.MallRepoKey, func(c di.Container) (any, error) {
+		return postgres.NewMallRepository(
+			constants.MallTableName,
+			postgresotel.Trace(c.Get(constants.DatabaseTransactionKey).(*sql.Tx)),
+		), nil
 	})
 	return
 }
